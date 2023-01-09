8b274d03d04917c839a64500dc71c78719a5ef3c
Carlo Bobba <carlo.bobba@outlook.it>
2023-01-09 21:07:58 +0100

    [2014-04-24] Gabriele: JMF_01 contiene i due esempi che abbiamo provato oggi in classe, con le librerie già aggiornate (Carlo e Erika sanno che fatica abbiamo fatto! Ahah)

---
+++ src/prove/TrivialJMFPlayer.java
@@ -0,0 +1,41 @@
+package prove;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FileDialog;
+import java.io.File;
+
+import javax.media.Manager;
+import javax.media.Player;
+import javax.swing.JFrame;
+
+public class TrivialJMFPlayer extends JFrame {
+	
+	public TrivialJMFPlayer() throws java.io.IOException, 
+									 java.net.MalformedURLException, 
+									 javax.media.MediaException {
+		FileDialog fd = new FileDialog(this, "TrivialJMFPlayer", FileDialog.LOAD);
+		fd.setVisible(true);
+		File fileVideo = new File(fd.getDirectory(), fd.getFile());
+		Player player = Manager.createRealizedPlayer(fileVideo.toURI().toURL());
+		Component c = player.getVisualComponent();
+		Component cpc = player.getControlPanelComponent();
+		if (cpc != null) {
+			add(cpc, BorderLayout.SOUTH);
+		}
+		add(c, BorderLayout.CENTER);
+
+		player.start();
+	}
+	
+	public static void main(String[] args) {
+		try {
+			JFrame f = new TrivialJMFPlayer();
+			f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			f.pack();
+			f.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/prove/VideoPlayerTest01.java
@@ -0,0 +1,105 @@
+package prove;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.URL;
+
+import javax.media.ControllerAdapter;
+import javax.media.ControllerListener;
+import javax.media.Manager;
+import javax.media.Player;
+import javax.media.RealizeCompleteEvent;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+public class VideoPlayerTest01 extends JFrame{
+
+	Player player;
+    Component center;
+    Component south;
+  
+    public VideoPlayerTest01() {
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      JButton button = new JButton("Select File");
+      ActionListener listener = 
+          new ActionListener() {
+        public void actionPerformed(
+            ActionEvent event) {
+          JFileChooser chooser = 
+            new JFileChooser(".");
+          int status = 
+            chooser.showOpenDialog(VideoPlayerTest01.this);
+          if (status == 
+              JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            try {
+              load(file);
+            } catch (Exception e) {
+              System.err.println("Try again: " + e);
+            }
+          }
+        }
+      };
+      button.addActionListener(listener);
+      getContentPane().add(button, 
+        BorderLayout.NORTH);
+      pack();
+      show();
+    }
+  
+    public void load(final File file) 
+        throws Exception {
+      URL url = file.toURI().toURL();
+      final Container contentPane = 
+        getContentPane();
+      if (player != null) {
+        player.stop();
+      }
+      player = Manager.createPlayer(url);
+      ControllerListener listener = 
+          new ControllerAdapter() {
+        public void realizeComplete(
+            RealizeCompleteEvent event) {
+          Component vc = 
+            player.getVisualComponent();
+          if (vc != null) {
+            contentPane.add(vc, 
+              BorderLayout.CENTER);
+            center = vc;
+          } else {
+            if (center != null) {
+              contentPane.remove(center);
+              contentPane.validate();
+            }
+          }
+          Component cpc = 
+            player.getControlPanelComponent();
+          if (cpc != null) {
+            contentPane.add(cpc, 
+              BorderLayout.SOUTH);
+            south = cpc;
+          } else {
+            if (south != null) {
+              contentPane.remove(south);
+              contentPane.validate();
+            }
+          }
+          pack();
+          setTitle(file.getName());
+        }
+      };
+      player.addControllerListener(listener);
+      player.start();
+    }
+  
+    public static void main(String args[]) {
+    	VideoPlayerTest01 pv = new VideoPlayerTest01();
+    }
+
+}
+		 
